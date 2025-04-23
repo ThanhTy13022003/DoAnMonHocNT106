6100bc92841ae0d5ec81f3987b84a8cf1d95c9a1
s3cr3t <96987990+BaoNguyenDuyQuoc@users.noreply.github.com>
2025-04-23 23:32:20 +0700

    Add files via upload

---
+++ Changepassword.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DoAnMonHocNT106
+{
+    partial class Changepassword
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelCurrent = new System.Windows.Forms.Label();
+            this.labelNew = new System.Windows.Forms.Label();
+            this.labelVerify = new System.Windows.Forms.Label();
+            this.textBoxCurrent = new System.Windows.Forms.TextBox();
+            this.textBoxNew = new System.Windows.Forms.TextBox();
+            this.textBoxVerify = new System.Windows.Forms.TextBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelCurrent
+            // 
+            this.labelCurrent.AutoSize = true;
+            this.labelCurrent.Location = new System.Drawing.Point(100, 50);
+            this.labelCurrent.Name = "labelCurrent";
+            this.labelCurrent.Size = new System.Drawing.Size(113, 16);
+            this.labelCurrent.TabIndex = 0;
+            this.labelCurrent.Text = "Current Password";
+            // 
+            // labelNew
+            // 
+            this.labelNew.AutoSize = true;
+            this.labelNew.Location = new System.Drawing.Point(100, 100);
+            this.labelNew.Name = "labelNew";
+            this.labelNew.Size = new System.Drawing.Size(95, 16);
+            this.labelNew.TabIndex = 1;
+            this.labelNew.Text = "New Password";
+            // 
+            // labelVerify
+            // 
+            this.labelVerify.AutoSize = true;
+            this.labelVerify.Location = new System.Drawing.Point(100, 150);
+            this.labelVerify.Name = "labelVerify";
+            this.labelVerify.Size = new System.Drawing.Size(124, 16);
+            this.labelVerify.TabIndex = 2;
+            this.labelVerify.Text = "Verify New Password";
+            // 
+            // textBoxCurrent
+            // 
+            this.textBoxCurrent.Location = new System.Drawing.Point(250, 47);
+            this.textBoxCurrent.Name = "textBoxCurrent";
+            this.textBoxCurrent.Size = new System.Drawing.Size(250, 22);
+            this.textBoxCurrent.TabIndex = 3;
+            this.textBoxCurrent.UseSystemPasswordChar = true;
+            // 
+            // textBoxNew
+            // 
+            this.textBoxNew.Location = new System.Drawing.Point(250, 97);
+            this.textBoxNew.Name = "textBoxNew";
+            this.textBoxNew.Size = new System.Drawing.Size(250, 22);
+            this.textBoxNew.TabIndex = 4;
+            this.textBoxNew.UseSystemPasswordChar = true;
+            // 
+            // textBoxVerify
+            // 
+            this.textBoxVerify.Location = new System.Drawing.Point(250, 147);
+            this.textBoxVerify.Name = "textBoxVerify";
+            this.textBoxVerify.Size = new System.Drawing.Size(250, 22);
+            this.textBoxVerify.TabIndex = 5;
+            this.textBoxVerify.UseSystemPasswordChar = true;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(250, 200);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(100, 30);
+            this.buttonChange.TabIndex = 6;
+            this.buttonChange.Text = "Change";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // Changepassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 300);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.textBoxVerify);
+            this.Controls.Add(this.textBoxNew);
+            this.Controls.Add(this.textBoxCurrent);
+            this.Controls.Add(this.labelVerify);
+            this.Controls.Add(this.labelNew);
+            this.Controls.Add(this.labelCurrent);
+            this.Name = "Changepassword";
+            this.Text = "Change Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCurrent;
+        private System.Windows.Forms.Label labelNew;
+        private System.Windows.Forms.Label labelVerify;
+        private System.Windows.Forms.TextBox textBoxCurrent;
+        private System.Windows.Forms.TextBox textBoxNew;
+        private System.Windows.Forms.TextBox textBoxVerify;
+        private System.Windows.Forms.Button buttonChange;
+    }
+}
---
+++ Informationgame.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace DoAnMonHocNT106
+{
+    partial class Informationgame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPlayerInfo = new System.Windows.Forms.GroupBox();
+            this.labelOpponentName = new System.Windows.Forms.Label();
+            this.labelOpponentScore = new System.Windows.Forms.Label();
+            this.labelYourScore = new System.Windows.Forms.Label();
+            this.labelYourName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBoxRoomInfo = new System.Windows.Forms.GroupBox();
+            this.labelRoomStatus = new System.Windows.Forms.Label();
+            this.labelRoomID = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBoxSystemInfo = new System.Windows.Forms.GroupBox();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelDeveloper = new System.Windows.Forms.Label();
+            this.labelReleaseDate = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBoxPlayerInfo.SuspendLayout();
+            this.groupBoxRoomInfo.SuspendLayout();
+            this.groupBoxSystemInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxPlayerInfo
+            // 
+            this.groupBoxPlayerInfo.Controls.Add(this.label7);
+            this.groupBoxPlayerInfo.Controls.Add(this.label6);
+            this.groupBoxPlayerInfo.Controls.Add(this.label5);
+            this.groupBoxPlayerInfo.Controls.Add(this.label4);
+            this.groupBoxPlayerInfo.Controls.Add(this.labelOpponentName);
+            this.groupBoxPlayerInfo.Controls.Add(this.labelOpponentScore);
+            this.groupBoxPlayerInfo.Controls.Add(this.labelYourScore);
+            this.groupBoxPlayerInfo.Controls.Add(this.labelYourName);
+            this.groupBoxPlayerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxPlayerInfo.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxPlayerInfo.Name = "groupBoxPlayerInfo";
+            this.groupBoxPlayerInfo.Size = new System.Drawing.Size(776, 150);
+            this.groupBoxPlayerInfo.TabIndex = 0;
+            this.groupBoxPlayerInfo.TabStop = false;
+            this.groupBoxPlayerInfo.Text = "Thông tin người chơi";
+            // 
+            // labelOpponentName
+            // 
+            this.labelOpponentName.AutoSize = true;
+            this.labelOpponentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOpponentName.Location = new System.Drawing.Point(200, 90);
+            this.labelOpponentName.Name = "labelOpponentName";
+            this.labelOpponentName.Size = new System.Drawing.Size(120, 18);
+            this.labelOpponentName.TabIndex = 3;
+            this.labelOpponentName.Text = "Nguyễn Văn B";
+            // 
+            // labelOpponentScore
+            // 
+            this.labelOpponentScore.AutoSize = true;
+            this.labelOpponentScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOpponentScore.Location = new System.Drawing.Point(500, 90);
+            this.labelOpponentScore.Name = "labelOpponentScore";
+            this.labelOpponentScore.Size = new System.Drawing.Size(32, 18);
+            this.labelOpponentScore.TabIndex = 2;
+            this.labelOpponentScore.Text = "150";
+            // 
+            // labelYourScore
+            // 
+            this.labelYourScore.AutoSize = true;
+            this.labelYourScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelYourScore.Location = new System.Drawing.Point(500, 40);
+            this.labelYourScore.Name = "labelYourScore";
+            this.labelYourScore.Size = new System.Drawing.Size(32, 18);
+            this.labelYourScore.TabIndex = 1;
+            this.labelYourScore.Text = "200";
+            // 
+            // labelYourName
+            // 
+            this.labelYourName.AutoSize = true;
+            this.labelYourName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelYourName.Location = new System.Drawing.Point(200, 40);
+            this.labelYourName.Name = "labelYourName";
+            this.labelYourName.Size = new System.Drawing.Size(119, 18);
+            this.labelYourName.TabIndex = 0;
+            this.labelYourName.Text = "Nguyễn Văn A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tên của bạn (Bạn là X):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(400, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Điểm của bạn:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Tên đối thủ (Đối thủ là O):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(400, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Điểm đối thủ:";
+            // 
+            // groupBoxRoomInfo
+            // 
+            this.groupBoxRoomInfo.Controls.Add(this.labelRoomStatus);
+            this.groupBoxRoomInfo.Controls.Add(this.labelRoomID);
+            this.groupBoxRoomInfo.Controls.Add(this.label8);
+            this.groupBoxRoomInfo.Controls.Add(this.label9);
+            this.groupBoxRoomInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxRoomInfo.Location = new System.Drawing.Point(12, 168);
+            this.groupBoxRoomInfo.Name = "groupBoxRoomInfo";
+            this.groupBoxRoomInfo.Size = new System.Drawing.Size(776, 120);
+            this.groupBoxRoomInfo.TabIndex = 1;
+            this.groupBoxRoomInfo.TabStop = false;
+            this.groupBoxRoomInfo.Text = "Thông tin phòng game";
+            // 
+            // labelRoomStatus
+            // 
+            this.labelRoomStatus.AutoSize = true;
+            this.labelRoomStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoomStatus.Location = new System.Drawing.Point(200, 70);
+            this.labelRoomStatus.Name = "labelRoomStatus";
+            this.labelRoomStatus.Size = new System.Drawing.Size(127, 18);
+            this.labelRoomStatus.TabIndex = 3;
+            this.labelRoomStatus.Text = "Đang chờ đối thủ";
+            // 
+            // labelRoomID
+            // 
+            this.labelRoomID.AutoSize = true;
+            this.labelRoomID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoomID.Location = new System.Drawing.Point(200, 30);
+            this.labelRoomID.Name = "labelRoomID";
+            this.labelRoomID.Size = new System.Drawing.Size(64, 18);
+            this.labelRoomID.TabIndex = 2;
+            this.labelRoomID.Text = "ROOM01";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(20, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 18);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Trạng thái:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(20, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 18);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Mã phòng:";
+            // 
+            // groupBoxSystemInfo
+            // 
+            this.groupBoxSystemInfo.Controls.Add(this.labelVersion);
+            this.groupBoxSystemInfo.Controls.Add(this.labelDeveloper);
+            this.groupBoxSystemInfo.Controls.Add(this.labelReleaseDate);
+            this.groupBoxSystemInfo.Controls.Add(this.label10);
+            this.groupBoxSystemInfo.Controls.Add(this.label11);
+            this.groupBoxSystemInfo.Controls.Add(this.label12);
+            this.groupBoxSystemInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxSystemInfo.Location = new System.Drawing.Point(12, 294);
+            this.groupBoxSystemInfo.Name = "groupBoxSystemInfo";
+            this.groupBoxSystemInfo.Size = new System.Drawing.Size(776, 144);
+            this.groupBoxSystemInfo.TabIndex = 2;
+            this.groupBoxSystemInfo.TabStop = false;
+            this.groupBoxSystemInfo.Text = "Thông tin hệ thống";
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVersion.Location = new System.Drawing.Point(200, 40);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(48, 18);
+            this.labelVersion.TabIndex = 5;
+            this.labelVersion.Text = "1.0.0";
+            // 
+            // labelDeveloper
+            // 
+            this.labelDeveloper.AutoSize = true;
+            this.labelDeveloper.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDeveloper.Location = new System.Drawing.Point(200, 70);
+            this.labelDeveloper.Name = "labelDeveloper";
+            this.labelDeveloper.Size = new System.Drawing.Size(209, 18);
+            this.labelDeveloper.TabIndex = 4;
+            this.labelDeveloper.Text = "Nhóm XYZ - Đồ án môn học";
+            // 
+            // labelReleaseDate
+            // 
+            this.labelReleaseDate.AutoSize = true;
+            this.labelReleaseDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReleaseDate.Location = new System.Drawing.Point(200, 100);
+            this.labelReleaseDate.Name = "labelReleaseDate";
+            this.labelReleaseDate.Size = new System.Drawing.Size(100, 18);
+            this.labelReleaseDate.TabIndex = 3;
+            this.labelReleaseDate.Text = "01/01/2023";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(20, 40);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 18);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Phiên bản:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(20, 70);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(104, 18);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Nhà phát triển:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(20, 100);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(106, 18);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Ngày phát hành:";
+            // 
+            // Informationgame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxSystemInfo);
+            this.Controls.Add(this.groupBoxRoomInfo);
+            this.Controls.Add(this.groupBoxPlayerInfo);
+            this.Name = "Informationgame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông tin game";
+            this.groupBoxPlayerInfo.ResumeLayout(false);
+            this.groupBoxPlayerInfo.PerformLayout();
+            this.groupBoxRoomInfo.ResumeLayout(false);
+            this.groupBoxRoomInfo.PerformLayout();
+            this.groupBoxSystemInfo.ResumeLayout(false);
+            this.groupBoxSystemInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPlayerInfo;
+        private System.Windows.Forms.Label labelOpponentName;
+        private System.Windows.Forms.Label labelOpponentScore;
+        private System.Windows.Forms.Label labelYourScore;
+        private System.Windows.Forms.Label labelYourName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBoxRoomInfo;
+        private System.Windows.Forms.Label labelRoomStatus;
+        private System.Windows.Forms.Label labelRoomID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBoxSystemInfo;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelDeveloper;
+        private System.Windows.Forms.Label labelReleaseDate;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
