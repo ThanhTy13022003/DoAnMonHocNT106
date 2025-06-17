4fd6b471549817c36505073a0558613603ffe624
s3cr3t <baongdqu@gmail.com>
2025-06-17 14:46:04 +0700

    Thay đổi tên 1 chút

--- Contribute_PlayViaLan/Properties/Resources.Designer.cs
+++ Contribute_PlayViaLan/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GameCaro_PlayViaLan.Properties {
+namespace DoAn_GameCaro_PlayViaLan.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GameCaro_PlayViaLan.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GameCaro_PlayViaLan.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DoAn_GameCaro_PlayViaLan.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
